81dff605e7b9376360cbeb65e77cdb01c1b5f69a
Gratien UWACU <gratienuwacu@gmail.com>
2025-04-25 20:24:46 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gratien UWACU PHP</title>
+</head>
+<body>
+    <h1>Gratien UWACU PHP</h1>
+
+    <pre>
+    ***********
+    **       **
+    **
+    **
+    **
+    **       **
+    ***********
+    </pre>
+
+    <p>The SHA256 hash of "Gratien UWACU" is:</p>
+    <p>
+        <?php
+            echo hash('sha256', 'Gratien UWACU');
+        ?>
+    </p>
+
+    <p>
+        <a href="check.php">Click here to check the error setting</a><br>
+        <a href="fail.php">Click here to cause a traceback</a>
+    </p>
+</body>
+</html>
